4193cb773406838a5d0c0fcb98b9ba8a211761f5
Gaibullaev Islam <Gaibullaeviz@gmail.com>
2019-08-14 14:37:26 +0300

    new design

--- js/main.js
+++ js/main.js
@@ -6,20 +6,20 @@ let btnw = document.getElementById('btn');
 btnw.addEventListener('click', function(){
   music.play();
   btnw.classList.remove('slideInRight');
+  btnw.style.opacity = '0';
+  btnws.style.opacity = '1';
   btnw.classList.add('slideOutRight');
   btnws.classList.remove('slideOutLeft');
   btnws.classList.add('slideInLeft');
-  lg.classList.add('animated');
-  lg.classList.add('jello');
-  lg.classList.add('infinite');
+  lg.classList.add('pulse');
 });
 btnws.addEventListener('click', function(){
   music.pause();
   btnws.classList.remove('slideInLeft');
   btnw.classList.remove('slideOutRight');
+  btnws.style.opacity = '0';
+  btnw.style.opacity = '1';
   btnws.classList.add('slideOutLeft');
   btnw.classList.add('slideInRight');
-  lg.classList.remove('animated');
-  lg.classList.remove('jello');
-  lg.classList.remove('infinite');
+  lg.classList.remove('pulse');
 });
